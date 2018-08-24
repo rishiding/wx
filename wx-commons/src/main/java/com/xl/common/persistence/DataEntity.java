5bd506f15913ab3d03787b5dff7c35dccb68e6da
rishi <qianfo_713@163.com>
2018-08-24 09:07:04 +0800

    时间少8小时修复

--- wx-commons/src/main/java/com/xl/common/persistence/DataEntity.java
+++ wx-commons/src/main/java/com/xl/common/persistence/DataEntity.java
@@ -86,7 +86,7 @@ public abstract class DataEntity<T> extends BaseEntity<T> {
 		this.createBy = createBy;
 	}
 
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
 	public Date getCreateDate() {
 		return createDate;
 	}
@@ -104,7 +104,7 @@ public abstract class DataEntity<T> extends BaseEntity<T> {
 		this.updateBy = updateBy;
 	}
 
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss" ,timezone="GMT+8")
 	public Date getUpdateDate() {
 		return updateDate;
 	}
